d8e20a082ba971bad588094ad92ead3ce5dea215
Suchoss <suchoss@gmail.com>
2022-12-27 17:02:04 +0100

    init

---
+++ PartialLog.Off/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using Serilog;
+
+namespace PartialLog.Off;
+
+public class MyClass
+{
+    public static void Hello()
+    {
+        Log.ForContext<MyClass>().Warning("Tohle je warning z {typeFullName}", typeof(MyClass).FullName);
+    }
+}
---
+++ SerilogFromLibPoc/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Filters;
+
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("serilog.json")
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .Filter.ByExcluding(Matching.FromSource("PartialLog.Off"))
+    .Filter.ByExcluding(Matching.FromSource("LibWithLoggingTurnedOff"))
+    .CreateLogger();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+
+Log.Information("Tohle je log z main classy!");
+
+LibWithLogging.MyClass.Hello();
+LibWithLoggingTurnedOff.MyClass.Hello();
+PartialLog.Off.MyClass.Hello();
+PartialLog.Off.Sub.MyClass.Hello();
+PartialLog.On.MyClass.Hello();
+
+
+Log.CloseAndFlush();
+Console.WriteLine("---konec---");
